9f0a2a72e6074d82e5abe3e30b207c4064801ebe
salzmasn <55196664+salzmasn@users.noreply.github.com>
2019-11-18 15:47:07 -0500

    Added Pages for device display and add a device
    device display can be done away with if need be

---
+++ src/app/components/devicedisplay/devicedisplay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-devicedisplay',
+    templateUrl: './devicedisplay.component.html',
+    styleUrls: ['./devicedisplay.component.css']
+})
+export class DeviceDisplayComponent {
+
+}
